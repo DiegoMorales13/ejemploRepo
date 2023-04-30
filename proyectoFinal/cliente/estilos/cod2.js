a6b37518dee995cccd5659406968f1292e33a01b
Carolina Rodriquez <cristar2002@hotmail.com>
2023-04-29 21:28:35 -0500

    actualización código proyecto

--- proyectoFinal/cliente/estilos/cod2.js
+++ proyectoFinal/cliente/estilos/cod2.js
@@ -6,3 +6,16 @@ btnRegresarPagina.addEventListener("click", volver);
 function volver() {
   window.open("index.html", "_self");
 }
+
+const btnDiligenciar = document.getElementById('btn-diligenciar');
+
+btnDiligenciar.addEventListener('click', () => {
+  const selectExamen = document.getElementById('select-examen');
+  const selectedOption = selectExamen.options[selectExamen.selectedIndex].value;
+
+  if (selectedOption === 'hemograma') {
+    window.open("hemograma.html", "_self");
+  } else if (selectedOption === 'uroanalisis') {
+    window.open("uroanalisis.html", "_self");
+  }
+});
