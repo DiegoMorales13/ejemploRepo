e862176b28fdc67fa60cc733885c7447d6a8a01d
Carolina Rodriquez <cristar2002@hotmail.com>
2023-05-03 22:13:29 -0500

    actualización proyecto

--- proyectoFinal/cliente/estilos/cod3.js
+++ proyectoFinal/cliente/estilos/cod3.js
@@ -6,3 +6,17 @@ returnpage.addEventListener("click", atras);
 function atras() {
   window.open("user2.html", "_self");
 }
+
+document.getElementById("myButton").addEventListener("click", function () {
+  // Deshabilita el botón para evitar múltiples clics
+  document.getElementById("myButton").disabled = true;
+
+  // Agrega el spinner de Bootstrap al botón
+  document.getElementById("myButton").innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...';
+
+  // Espera 5 segundos antes de abrir la nueva página
+  setTimeout(function () {
+    window.location.href = "https://www.google.com";
+  }, 5000);
+});
+
